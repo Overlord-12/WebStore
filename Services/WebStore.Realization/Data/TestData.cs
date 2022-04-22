7e5e3bd3c9dcdd8ccce0345fda23969f12df1224
Danila Argastesv <danya.argastsev.02@mail.ru>
2022-04-22 18:49:01 +0300

    Deleted data

--- Services/WebStore.Realization/Data/TestData.cs
+++ Services/WebStore.Realization/Data/TestData.cs
@@ -9,7 +9,6 @@ public class TestData
     {
         new () { Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 23 },
         new () { Id = 2, LastName = "Петров", FirstName = "Пётр", Patronymic = "Петрович", Age = 27 },
-        new () { Id = 3, LastName = "Сидоров", FirstName = "Сидор", Patronymic = "Сидорович", Age = 18 },
     };
 
     /// <summary>Секции</summary>
